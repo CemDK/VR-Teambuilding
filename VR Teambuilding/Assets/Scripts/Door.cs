ab8e7b8ad6c5ed0429a7561673cf7c3b1956599d
Cem Deniz Kabakci <25245902+CemDK@users.noreply.github.com>
2019-01-03 22:42:15 +0100

    Unity Project files

---
+++ VR Teambuilding/Assets/Scripts/Door.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour{
+
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+    private bool open = false;
+    private bool disabled = false;
+    private Vector3 left = new Vector3(0, 0, -0.01f);
+    private Vector3 right = new Vector3(0, 0, 0.01f);
+
+
+    public void Open() {
+        if (open) {
+            StartCoroutine("SlideClosed");
+        } else {
+            StartCoroutine("SlideOpen");
+        }
+        this.GetComponent<BoxCollider>().enabled = !this.GetComponent<BoxCollider>().enabled;
+        open = !open;
+    }
+
+    IEnumerator SlideClosed() {
+        for (float f = 0; f < 99; f++) {
+            leftDoor.transform.Translate(right);
+            rightDoor.transform.Translate(left);
+            yield return new WaitForSeconds(.01f);
+        }
+    }
+
+    IEnumerator SlideOpen() {
+        for (float f = 0; f < 99; f++) {
+            leftDoor.transform.Translate(left);
+            rightDoor.transform.Translate(right);
+            yield return new WaitForSeconds(.01f);
+        }   
+    }
+
+
+
+}
---
+++ VR Teambuilding/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class DoorOpener : MonoBehaviour{
+
+    private VRTK_InteractableObject _linkedObject;
+    public GameObject target;
+
+    void Awake(){
+        _linkedObject = GetComponent<VRTK_InteractableObject>();
+        _linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+    }
+
+
+    private void InteractableObjectUsed(object sender, InteractableObjectEventArgs e){
+        OpenDoor();
+    }
+
+    public void OpenDoor() {
+        //target.GetComponent<Renderer>().material.color = Random.ColorHSV(1f, 1f);
+        target.GetComponent<Door>().Open();
+    }
+}
