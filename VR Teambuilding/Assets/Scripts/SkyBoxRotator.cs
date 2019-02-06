69cb9f02f70affbf843bb2088c089d75d7d5e5e3
Cem Deniz Kabakci <25245902+CemDK@users.noreply.github.com>
2019-02-06 22:27:05 +0100

    Added Rotation to SkyBox

---
+++ VR Teambuilding/Assets/Scripts/SkyBoxRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxRotator : MonoBehaviour
+{
+    // Speed multiplier
+    [SerializeField] private float speedMultiplier;
+
+    void Update() {
+        //Sets the float value of "_Rotation", adjust it by Time.time and a multiplier.
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speedMultiplier);
+    }
+}
